f5139aa9388f36fd14f557c6e2bb67e76b293a63
kazuma-48 <203885136wm@gmail.com>
2025-07-02 11:42:55 +0900

    ホーム画面

--- QuezApp.java
+++ QuezApp.java
@@ -8,11 +8,38 @@ public class QuezApp {
         String html = """
                 <html>
                 <head>
-                    <meta charset = "UTF-8">
+                    <meta charset = 'UTF-8'>
                     <title>クイズアプリ</title>
+                    <script>
+                        function goToNext() {
+                            document.getElementById('main').style.display = 'none';
+                            document.getElementById('next').style.display = 'block';
+                        }
+                        function goBack() {
+                            document.getElementById('main').style.display = 'block';
+                            document.getElementById('next').style.display = 'none';
+                        }
+                    </script>
                 </head>
                 <body>
-                    <button onclick="alert('クリックされました！')">クリック</button>
+                    <div id='main'>
+                        <table style='margin:auto;'>
+                            <tr>
+                                <td><button onclick='goToNext()' style='font-size: 100px;'>歴史</button></td>
+                                <td><button onclick='goToNext()' style='font-size: 100px;'>地理</button></td>
+                                <td><button onclick='goToNext()' style='font-size: 100px;'>雑学</button></td>
+                            </tr>
+                            <tr>
+                                <td><button onclick='goToNext()' style='font-size: 100px;'></button></td>
+                                <td><button onclick='goToNext()' style='font-size: 100px;'>文学</button></td>
+                                <td><button onclick='goToNext()' style='font-size: 100px;'>芸術</button></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id='next' style='display:none;'>
+                        <h2>これは2つ目の画面です</h2>
+                        <button onclick='goBack()'>戻る</button>
+                    </div>
                 </body>
                 </html>
                 """;
