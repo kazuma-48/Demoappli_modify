d5a249e463b6433f70b6b781f7669d14f8b0ad92
kazuma-48 <203885136wm@gmail.com>
2025-07-08 14:08:02 +0900

    クイズ

--- QuezApp/QuezMain.java
+++ QuezApp/QuezMain.java
@@ -1,4 +1,5 @@
 package QuezApp;
+
 import java.awt.Desktop;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,12 +13,14 @@ public class QuezMain {
                 html.append("<div id='main'>\n<table style='margin:auto;'><tr>");
                 html.append("<td><button onclick=\"goToNext('historyGenreScreen')\" style='font-size: 100px;'>歴史</button></td>");
                 html.append("<td><button onclick=\"goToNext('geoScreen')\" style='font-size: 100px;'>地理</button></td>");
+                html.append("<td><button onclick=\"goToNext('englishScreen')\" style='font-size:100px; margin:30px;'>英語</button></td>");
                 html.append("</tr></table>\n</div>");
                 // 歴史ジャンル選択画面
                 html.append("<div id='historyGenreScreen' style='display:none; text-align:center;'>");
                 html.append("<h2>歴史ジャンルを選択</h2>");
                 html.append("<button onclick=\"goToNext('nihonshiScreen')\" style='font-size:60px; margin:30px;'>日本史</button>");
                 html.append("<button onclick=\"goToNext('worldshiScreen')\" style='font-size:60px; margin:30px;'>世界史</button>");
+
                 html.append("<br><button class='home-btn' onclick=\"goBack('historyGenreScreen')\">ホームに戻る</button>");
                 html.append("</div>");
                 // 日本史クイズ画面
@@ -31,6 +34,11 @@ public class QuezMain {
                 html.append("<div id='worldshiScreen' style='display:none;'><h2>世界史クイズは準備中です</h2><button class='home-btn' onclick=\"goBack('worldshiScreen')\">ジャンル選択に戻る</button></div>");
                 // 地理画面のダミー
                 html.append("<div id='geoScreen' style='display:none;'><h2>地理クイズは準備中です</h2><button onclick=\"goBack('geoScreen')\">ホームに戻る</button></div>");
+                // 英語クイズ画面
+                html.append("<div id='englishScreen' style='display:none;'>");
+                String englishQuizHtml = English.getQuizHtml();
+                html.append(englishQuizHtml);
+                html.append("</div>");
                 // 日本史ジャンルから問題画面に遷移するJS
                 html.append("<script>\n" +
                                 "function goToNext(id) {\n" +
